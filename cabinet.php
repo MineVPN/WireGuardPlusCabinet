ec9809184a66d459e97658d54e05472ff219eca1
MineVPN <122735606+MineVPN@users.noreply.github.com>
2025-07-10 16:20:19 +0300

    Update cabinet.php

--- cabinet.php
+++ cabinet.php
@@ -1,7 +1,6 @@
 <?php
-session_start(); // Начало сессии
+session_start();
 
-// Проверяем, установлена ли сессия
 if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] !== true) {
     header("Location: login.php");
     exit();
@@ -11,55 +10,48 @@ if(isset($_POST['menu'])){
     $_GET['menu'] = $_POST['menu'];
 }
 
-// По умолчанию открывается страница VPN
-$menu_item = isset($_GET['menu']) ? $_GET['menu'] : 'vpn';
+$menu_item = isset($_GET['menu']) ? $_GET['menu'] : 'vpn_manager';
 
-// Пути к страницам меню
 $menu_pages = [
-    'vpn' => 'vpn.php', // Изменили на общий vpn.php
-    'ping' => 'pinger.php',
-    'route' => 'route.php'
+    'vpn_manager' => 'vpn_manager.php',
+    'route' => 'route.php',
+    'ping' => 'pinger.php'
 ];
 
 if (!array_key_exists($menu_item, $menu_pages)) {
-    $menu_item = 'vpn';
+    $menu_item = 'vpn_manager';
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="favicon.png">
-    <title>VPN+</title>
+    <title>WireGuard+ Cabinet</title>
     <link rel="stylesheet" href="styles.css">
     <script>
     function Notice(text) {
-        var elements = document.querySelectorAll('.notice');
-        elements.forEach(function(element) {
+        const element = document.querySelector('.notice');
+        if (element) {
             element.textContent = text;
-            if (element.classList.contains('hidden')) {
-                element.classList.remove('hidden');
-            }
-        });
+            element.classList.remove('hidden');
+        }
     }
     </script>
 </head>
 <body>
     <div class="sidebar">
         <img src="logo.png" class="logo">
-        <a class="menu-item" href="cabinet.php?menu=vpn">VPN Status</a>
-        <a class="menu-item" href="cabinet.php?menu=route">Route</a>
+        <a class="menu-item" href="cabinet.php?menu=vpn_manager">Управление VPN (wg1)</a>
+        <a class="menu-item" href="cabinet.php?menu=route">Маршруты обхода</a>
         <a class="menu-item" href="cabinet.php?menu=ping">Ping</a>
         <a class="menu-item" href="logout.php">Выход</a>
     </div>
 
     <div class="notice hidden"></div>
     <div class="page">
-        <?php
-        include_once $menu_pages[$menu_item];
-        ?>
+        <?php include_once $menu_pages[$menu_item]; ?>
     </div>
 </body>
 </html>
